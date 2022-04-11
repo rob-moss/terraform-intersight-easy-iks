8022ee74f91308707e4f1e27179dd681e30e4210
Rob Moss <romoss@cisco.com>
2022-04-11 14:39:44 +1000

    fix organization data_sources manually entered

--- modules/kubernetes_cluster_profiles/data_sources.tf
+++ modules/kubernetes_cluster_profiles/data_sources.tf
@@ -11,27 +11,27 @@ data "terraform_remote_state" "local_policies" {
   }
 }
 
-data "terraform_remote_state" "remote_policies" {
-  for_each = { for k, v in local.tfc_workspaces : k => v if v.backend == "remote" }
-  backend  = each.value.backend
-  config = {
-    organization = each.value.organization
-    workspaces = {
-      name = each.value.workspace
-    }
-  }
-}
-
-
 #data "terraform_remote_state" "remote_policies" {
-#  backend = "remote"
+#  for_each = { for k, v in local.tfc_workspaces : k => v if v.backend == "remote" }
+#  backend  = each.value.backend
 #  config = {
-#    organization = var.organization
+#    organization = each.value.organization
 #    workspaces = {
-#      name = var.workspaces
+#      name = each.value.workspace
 #    }
 #  }
 #}
+#
+
+data "terraform_remote_state" "remote_policies" {
+  backend = "remote"
+  config = {
+    organization = "Cisco-IST-TigerTeam"
+    workspaces = {
+      name = "romoss-iksworkshop_kubernetes_policies"
+    }
+  }
+}
 
 
 #data "terraform_remote_state" "remote_policies" {
